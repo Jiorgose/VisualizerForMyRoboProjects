dd5e7a6c98356e3eafc02aaedac6647020ae6b50
isuckatgamedev <jiorgose@gmail.com>
2025-05-29 19:56:33 +0200

    ImGui test window

--- PhysicsPredictorForMyRoboProjects/main.cpp
+++ PhysicsPredictorForMyRoboProjects/main.cpp
@@ -2,15 +2,17 @@
 #include <glad/glad.c>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <iostream>
 
 #include "eventsUtility.hpp"
 
+static const double pi = 2 * acos(0.0);
+
 static const int width = 800;
 static const int height = 600;
-
-static const double pi = 2 * acos(0.0);
 
 int main()
 {
@@ -27,12 +29,32 @@ int main()
   glfwSwapInterval(1);
   glfwSetKeyCallback(window, keyCallback);
 
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void)io;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+  ImGui::StyleColorsDark();
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 460");
+
   bool fullscreen = false;
   glfwSetWindowUserPointer(window, &fullscreen);
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
     
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Begin("test");
+    ImGui::Text("Hoi :)");
+    ImGui::End();
+
     double t = glfwGetTime();
 
     const float red = (float)(0.5 + 0.5 * sin(t));
@@ -42,12 +64,17 @@ int main()
     glClearColor(red, green, blue, 1.0); 
 
     glClear(GL_COLOR_BUFFER_BIT);
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     glfwSwapBuffers(window);
   }
 
-  glfwDestroyWindow(window);
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
 
+  glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
 }
