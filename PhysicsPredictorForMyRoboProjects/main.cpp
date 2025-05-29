54d08963e0e6c9bf4c330ca56bb4ba5340df71eb
isuckatgamedev <jiorgose@gmail.com>
2025-05-29 12:50:45 +0200

    opening a window w. glfw3

--- PhysicsPredictorForMyRoboProjects/main.cpp
+++ PhysicsPredictorForMyRoboProjects/main.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
 
 int main()
 {
-  std::cout << "yay its working" << std::endl;
+  glfwInit();
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Physics Predictor", NULL, NULL);
+
+  glfwMakeContextCurrent(window);
+
+  bool running = true;
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
 }
