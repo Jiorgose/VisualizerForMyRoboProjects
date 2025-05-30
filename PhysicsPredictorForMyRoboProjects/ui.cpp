5384725d7c707d15148dc7285ff545c078a0e95f
isuckatgamedev <jiorgose@gmail.com>
2025-05-30 15:59:28 +0200

    fullscreen docking, texture is still a bit cursed

--- PhysicsPredictorForMyRoboProjects/ui.cpp
+++ PhysicsPredictorForMyRoboProjects/ui.cpp
@@ -9,11 +9,52 @@ void uiNewFrame()
 
 void uiUpdate(int& fps, double dt, int width, int height, GLuint textureId)
 {
-  ImGui::Begin("test");
-  ImGui::Text("Hoi :)");
+  static ImGuiDockNodeFlags dockspaceFlags = ImGuiDockNodeFlags_PassthruCentralNode;
+
+  ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoDocking;
+
+  ImGuiViewport* viewport = ImGui::GetMainViewport();
+  ImGui::SetNextWindowPos(viewport->Pos);
+  ImGui::SetNextWindowSize(viewport->Size);
+  ImGui::SetNextWindowViewport(viewport->ID);
+  ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+  ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+  windowFlags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+  windowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+  windowFlags |= ImGuiWindowFlags_NoBackground;
+
+  ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+  ImGui::Begin("DockSpace", nullptr, windowFlags);
+  ImGui::PopStyleVar();
+  ImGui::PopStyleVar(2);
+
+  ImGuiID dockspaceId = ImGui::GetID("MyDockSpace");
+  ImGui::DockSpace(dockspaceId, ImVec2(0.0f, 0.0f), dockspaceFlags);
+
+  static auto firstTime = true;
+  if (firstTime) {
+    firstTime = false;
+
+    ImGui::DockBuilderRemoveNode(dockspaceId);
+    ImGui::DockBuilderAddNode(dockspaceId, dockspaceFlags | ImGuiDockNodeFlags_DockSpace);
+    ImGui::DockBuilderSetNodeSize(dockspaceId, viewport->Size);
+    auto rightId = ImGui::DockBuilderSplitNode(dockspaceId, ImGuiDir_Right, 0.25f, nullptr, &dockspaceId);
+
+    ImGui::DockBuilderDockWindow("Scene", dockspaceId);
+    ImGui::DockBuilderDockWindow("Settings", rightId);
+
+    ImGui::DockBuilderFinish(dockspaceId);
+    ImGui::DockBuilderFinish(dockspaceId);
+  }
+  ImGui::End();
+
+  ImGui::Begin("Scene");
+  ImGui::Image((ImTextureID)(intptr_t)textureId, ImVec2(width, height), ImVec2(0, 1), ImVec2(1, 0));
+  ImGui::End();
+
+  ImGui::Begin("Settings");
   ImGui::SliderInt("fps", &fps, 1, 120, nullptr, 0);
   ImGui::Value("fps", static_cast<float>(1.0 / dt));
-  ImGui::Image((ImTextureID)(intptr_t)textureId, ImVec2(width, height), ImVec2(0, 1), ImVec2(1, 0));
   ImGui::End();
 }
 
@@ -31,8 +72,17 @@ void uiInit(GLFWwindow* window)
   (void)io;
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
   ImGui::StyleColorsDark();
+
+  ImGuiStyle& style = ImGui::GetStyle();
+
+  style.TabRounding = 0.0f;
+  style.WindowRounding = 0.0f;
+  style.TabBorderSize = 0.0f;
+  style.WindowBorderSize = 0.0f;
+  style.FrameBorderSize = 0.0f;
 
   ImGui_ImplGlfw_InitForOpenGL(window, true);
   ImGui_ImplOpenGL3_Init("#version 460");
--- PhysicsPredictorForMyRoboProjects/ui.hpp
+++ PhysicsPredictorForMyRoboProjects/ui.hpp
@@ -2,6 +2,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <imgui.h>
+#include <imgui_internal.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
