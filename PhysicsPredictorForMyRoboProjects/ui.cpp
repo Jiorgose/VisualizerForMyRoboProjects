e1c1ffdcff9888680ff99659666bc91246ed792e
isuckatgamedev <jiorgose@gmail.com>
2025-06-02 11:29:15 +0200

    removed the shader again. Try again tomorrow

--- PhysicsPredictorForMyRoboProjects/ui.cpp
+++ PhysicsPredictorForMyRoboProjects/ui.cpp
@@ -61,7 +61,7 @@ void uiUpdate(GLuint textureId, GLuint fragmentShader, GLFWwindow* window)
 
   ImGui::Begin("Settings");
   ImGui::SliderInt("fps", &state->fps, 1, 120, nullptr, 0);
-  ImGui::Value("fps", static_cast<float>(1.0 / state->dt));
+  ImGui::Value("fps", static_cast<int>(round(1.0 / state->dt)));
   ImGui::ColorEdit3("color", state->color, 0);
   ImGui::DragFloat3("position", state->cameraPosition, 0);
   ImGui::End();
